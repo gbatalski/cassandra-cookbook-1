d763b51cabb07527a95d38c9e9dbcf18fd8ede51
Ghenadii Batalski <Ghenadii.Batalski@student.kit.edu>
2012-05-28 15:33:20 +0200

    corrected some config files

--- attributes/cassandra.rb
+++ attributes/cassandra.rb
@@ -1,7 +1,6 @@
 ### GENERAL
 #
 default[:cassandra][:apt][:uri]           = "http://www.apache.org/dist/cassandra/debian"
-default[:cassandra][:apt][:sources]       = %w[deb deb-src]
 default[:cassandra][:apt][:distributions] = %w[11x]
 default[:cassandra][:apt][:keyserver]     = "pgp.mit.edu"
 default[:cassandra][:apt][:key]           = "F758CE318D77295D"
--- recipes/apt.rb
+++ recipes/apt.rb
@@ -2,7 +2,6 @@
 # http://wiki.apache.org/cassandra/DebianPackaging
 apt_repository "cassandra" do
   uri node[:cassandra][:apt][:uri]
-  sources node[:cassandra][:apt][:sources]
   distributions node[:cassandra][:apt][:distributions]
   keyserver node[:cassandra][:apt][:keyserver]
   key node[:cassandra][:apt][:key]
